e27d57043eb0925e6a64f211ea8715dac1ba9dbe
Azizbek Odilov <azizbekodilov296@gmail.com>
2025-07-03 21:21:18 +0500

    Configured Railway DB correctly

--- zapchast/settings.py
+++ zapchast/settings.py
@@ -91,15 +91,9 @@ REST_FRAMEWORK = {
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 
 
